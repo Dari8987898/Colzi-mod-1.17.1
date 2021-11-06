cdcf3f5ce9d91f94d8cd312e5986d57b93203d4e
Dari8_ <dariuzzolo58@gmail.com>
2021-11-06 16:21:38 +0100

    Aggiunto 'Colzite Ore' con texture + traduzioni en e it

--- src/main/java/dariomegaganzo/colzimod/ColziMod.java
+++ src/main/java/dariomegaganzo/colzimod/ColziMod.java
@@ -1,7 +1,13 @@
 package dariomegaganzo.colzimod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.impl.object.builder.BlockSettingsInternals;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -16,11 +22,14 @@ public class ColziMod implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger("modid");
 
 	public static final ColziItem COLZI = new ColziItem(new FabricItemSettings().group(ItemGroup.MISC));
-	public static final ColziteIngotItem COLZITEINGOT = new ColziteIngotItem(new FabricItemSettings().group(ItemGroup.MATERIALS));
+	public static final ColziteIngotItem COLZITE_INGOT = new ColziteIngotItem(new FabricItemSettings().group(ItemGroup.MATERIALS));
+	public static final Block COLZITE_ORE = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("colzimod", "colzi"), COLZI);
-		Registry.register(Registry.ITEM, new Identifier("colzimod", "colzite_ingot"), COLZITEINGOT);
+		Registry.register(Registry.ITEM, new Identifier("colzimod", "colzite_ingot"), COLZITE_INGOT);
+		Registry.register(Registry.BLOCK, new Identifier("colzimod", "colzite_ore"), COLZITE_ORE);
 	}
 }
---
+++ src/main/java/dariomegaganzo/colzimod/ColziteOre.java
@@ -0,0 +1,9 @@
+package dariomegaganzo.colzimod;
+
+import net.minecraft.block.Block;
+
+public class ColziteOre extends Block{
+    public ColziteOre(Settings settings) {
+        super(settings);
+    } 
+}
