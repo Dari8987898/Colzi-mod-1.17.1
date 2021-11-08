50f4ef3f58cff2b61d21a03faf584d60136d9b98
Dari8_ <dariuzzolo58@gmail.com>
2021-11-08 20:23:03 +0100

    Aggiunto spawn casuale nel mondo del blocco 'Colzite Ore'

--- src/main/java/dariomegaganzo/colzimod/ColziMod.java
+++ src/main/java/dariomegaganzo/colzimod/ColziMod.java
@@ -2,6 +2,8 @@ package dariomegaganzo.colzimod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -11,7 +13,17 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolItem;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,10 +42,13 @@ public class ColziMod implements ModInitializer {
 	//BLOCKS
 	public static final Block COLZITE_ORE = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool());
 
-
+	//WORLD GENERATION
+	private static ConfiguredFeature<?, ?> COLZITE_ORE_OVERWORLD = Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, COLZITE_ORE.getDefaultState(), 4)).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.fixed(0),YOffset.fixed(64)))).spreadHorizontally().repeat(10));
 
 	@Override
 	public void onInitialize() {
+
+
 		//ITEMS
 		Registry.register(Registry.ITEM, new Identifier("colzimod", "colzi"), COLZI);
 		Registry.register(Registry.ITEM, new Identifier("colzimod", "colzite_ingot"), COLZITE_INGOT);
@@ -44,5 +59,10 @@ public class ColziMod implements ModInitializer {
 			Registry.register(Registry.BLOCK, new Identifier("colzimod", "colzite_ore"), COLZITE_ORE);
 			Registry.register(Registry.ITEM, new Identifier("colzimod", "colzite_ore"), new BlockItem(COLZITE_ORE, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		}
+
+		//WORLD GENERATION
+		RegistryKey<ConfiguredFeature<?, ?>> colziteOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier("colzimod", "colzite_ore"));
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, colziteOreOverworld.getValue(), COLZITE_ORE_OVERWORLD);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, colziteOreOverworld);
 	}
 }
